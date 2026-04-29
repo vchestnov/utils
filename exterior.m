d8b6489539758b446de66664ddf3095b8914a8c2
Seva Chestnov <vchestnov@gmail.com>
2026-04-29 18:23:39 +0100

    Refactor exterior.m
    Assisted by codex

--- exterior.m
+++ exterior.m
@@ -1,46 +1,80 @@
 (* ::Package:: *)
 
-Get["utils.m"]
+BeginPackage["exterior`"]
 
-(* ::Subsubsection::Closed:: *)
-(*Exterior algebra*)
+Get[FileNameJoin[{DirectoryName[$InputFileName], "utils.m"}]];
 
-ClearAll[d];
-d[x_ w:(_Wedge | _d)] := Wedge[d[x] // Collect[#, _d]&, w];
-(*Alternative*)
-(*d[x_ w:(_Wedge | _d)] := Wedge[d[x] // Expand, w];*)
-d[x_Plus] := x // Map[d];
-d[x_?NumericQ | x_d] := 0;
-d[Power[x_, n_]] := n Power[x, n - 1] d[x] + Power[x, n] Log[x] d[n];
-d[h_[xs__]] /; !MemberQ[{Wedge, d, Pattern}, h] && FreeQ[{xs}, Pattern] := {xs} // RightComposition[
-    Length,
-    IdentityMatrix,
-    Map[Apply[Derivative[##][h][xs]&]],
-    Dot[#, {xs} // Map[d]]&,
-    Identity
+d::usage = "d[expr] computes the exterior derivative of `expr`, distributing over sums and using the product and chain rules on expressions built from `wedge` and scalar functions.";
+wedge::usage = "wedge[x1, x2, ...] represents the exterior, or wedge, product. It is multilinear and has attributes `Flat` and `OneIdentity`.";
+sortWedge::usage = "sortWedge[w] rewrites a wedge product into sorted order and multiplies by the corresponding signature.";
+cleanWedge::usage = "cleanWedge[expr] expands wedge-product arguments, sorts each wedge factor canonically, and collects the result by wedge monomials.";
+
+Begin["`Private`"]
+
+With[
+    {
+        dHead = Symbol["exterior`d"],
+        wedgeHead = Symbol["exterior`wedge"]
+    },
+    (* Exterior derivative. *)
+    ClearAll[dHead];
+    dHead[Times[x_, w:(_wedgeHead | _dHead)]] := wedgeHead[Collect[dHead[x], _dHead], w];
+    dHead[x_Plus] := Map[dHead, x];
+    dHead[x_?NumericQ | x_dHead] := 0;
+    dHead[Power[x_, n_]] := n Power[x, n - 1] dHead[x] + Power[x, n] Log[x] dHead[n];
+    dHead[h_[xs__]] /; !MemberQ[{wedgeHead, dHead, Pattern}, h] && FreeQ[{xs}, Pattern] := {xs} // RightComposition[
+        Length,
+        IdentityMatrix,
+        Map[Apply[Derivative[##][h][xs]&]],
+        Dot[#, Map[dHead, {xs}]]&,
+        Identity
+    ];
+
+    (* Exterior product. *)
+    ClearAll[wedgeHead];
+    SetAttributes[wedgeHead, {Flat, OneIdentity}];
+    wedgeHead[xs___, x_Plus, ys___] := Map[wedgeHead[xs, #, ys]&, x];
+    wedgeHead[xs___, Times[n_, x:(_dHead | _wedgeHead)], ys___] := n wedgeHead[xs, x, ys];
+    wedgeHead[___, 0, ___] := 0;
+    wedgeHead[xs___, x_Times, ys___] := wedgeHead[xs, Expand[x], ys];
+    Format[wedgeHead[xs__]] := {xs} // Map[InputForm] // Riffle[#, "/\\"]& // utils`MkString["(", ##, ")"]&;
+    Format[wedgeHead[xs__], StandardForm] := Interpretation[System`Wedge[xs], wedgeHead[xs]];
+    Format[wedgeHead[xs__], OutputForm] := {xs} // Map[ToString[#, OutputForm]&] // Riffle[#, " /\\ "]& // utils`MkString;
+    (* Alternative notebook-style display using the generic ASCII formatter instead. *)
+    (* Format[wedgeHead[xs__], StandardForm] := {xs} // Map[InputForm] // Riffle[#, "/\\"]& // utils`MkString["(", ##, ")"]&; *)
+    (* Alternative terminal display with a Unicode wedge symbol instead of ASCII. *)
+    (* Format[wedgeHead[xs__], OutputForm] := {xs} // Map[ToString[#, OutputForm]&] // Riffle[#, " \[Wedge] "]& // utils`MkString; *)
+
+    (* Canonicalize a single wedge product by sorting its factors. *)
+    ClearAll[sortWedge];
+    sortWedge[w_wedgeHead] := Times[Signature[w], Apply[wedgeHead, Sort[w]]];
+
+    (* Expand, canonicalize, and collect wedge expressions. *)
+    ClearAll[cleanWedge];
+    cleanWedge := RightComposition[
+        ReplaceAll[w_wedgeHead :> Map[Expand, w]],
+        ReplaceAll[
+            RuleDelayed[
+                w_wedgeHead,
+                Times[Signature[w], Apply[wedgeHead, Sort[w]]]
+            ]
+        ],
+        Collect[#, _wedgeHead]&,
+        Identity
+    ];
 ];
 
-ClearAll[Wedge];
-(* TODO: investigate behavior of `Wedge` inside patterns *)
-SetAttributes[Wedge,{Flat, OneIdentity}];
-Wedge[xs___, x_Plus, ys___] := x // Map[Wedge[xs, #, ys]&]
-Wedge[xs___, n_ x:(_d | _Wedge), ys___] := n Wedge[xs, x, ys];
-Wedge[___, 0, ___] := 0;
-Wedge[xs___, x_Times, ys___] := Wedge[xs, x // Expand, ys];
-(*Format[Wedge[d[x_]]] := Wedge["", d[x]];*)
-(*Wedge[xs__] /; UnsameQ[{xs} // Sort, {xs}] := {xs} // Times[*)
-(*    # // Signature,                                         *)
-(*    # // Sort // Apply[Wedge]                               *)
-(*]&;                                                         *)
-(* Format[Wedge[xs__]] := {xs} // Map[InputForm] // Apply[MkString["(", ##, ")"]&]; *)
-Format[Wedge[xs__]] := {xs} // Map[InputForm] // Riffle[#, "/\\"]& // MkString["(", ##, ")"]&;
-sortWedge[w_Wedge] := w // Times[# // Signature, # // Sort]&;
-cleanWedge := RightComposition[
-    ReplaceAll[w_Wedge :> Map[Expand, w]],
-    ReplaceAll @ RuleDelayed[
-        w_Wedge,
-        w // Times[# // Signature, # // Sort]&
-    ],
-    Collect[#, _Wedge]&,
-    Identity
+
+SetAttributes[
+    {
+        d,
+        wedge,
+        sortWedge,
+        cleanWedge
+    },
+    ReadProtected
 ];
+
+End[]
+
+EndPackage[]
