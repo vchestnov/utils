75c4c81f2afc5cd41c698d07e587f8c9a640d490
Seva Chestnov <vchestnov@gmail.com>
2026-04-29 15:54:15 +0100

    Refactor and cosmetics
    Assisted by codex

--- fftensor.m
+++ fftensor.m
@@ -1,168 +1,93 @@
-(* ::Subsubsection::Closed:: *)
-(*tools for companion matrices*)
+(* ::Package:: *)
 
-getLC[poly_] := Coefficient[poly, z, Exponent[poly, z]];
+BeginPackage["fftensor`"]
 
-ClearAll[stdjs, cycles, cmats, cmatdz];
-stdjs[poly_] := stdjs[poly] = poly // Exponent[#, z]& // Range[0, # - 1]& // Map[j];
-(* `m`-cycles of the list `x`. Try
- *  Alphabet[] // cycles[5]
- *)
-cycles[m_Integer][x:(_List | _SparseArray)] := Range[0, m - 1] // RightComposition[
-    Map[RotateLeft[x, #]&],
-    Transpose,
-    Part[#, ;;Length[x] - m + 1]&,
-    Identity
-];
-cmats[poly_] := cmats[poly] = poly // RightComposition[
-    Exponent[#, z]&,
-    z^Range[0, 2 # - 2]&,
-    Map[FQuolyMod[#, poly, z]&],
-    subjs[z],
-    CoefficientArrays[#, stdjs[poly]]&,
-    Last,
-    cycles[# // Dimensions // Last][#]&,
-    Transpose[#, {1, 3, 2}]&,
-    Identity
-];
-cmatdz[poly_] := cmatdz[poly] = poly // RightComposition[
-    stdjs,
-    ReplaceAll[j[n_] :> z^n],
-    D[#, z]&,
-    subjs[z],
-    CoefficientArrays[#, stdjs[poly]]&,
-    Last,
-    (* edge case for linear `poly` *)
-    If[# // Dimensions // Length // # < 2&, {#} // SparseArray, #]&,
-    Transpose,
-    Identity
-];
+Get[FileNameJoin[{DirectoryName[$InputFileName], "utils.m"}]];
 
-(* ::Subsubsection::Closed:: *)
-(*block Toeplitz matrices*)
+blockmat::usage = "blockmat[ms] builds a block Toeplitz matrix from a rank-3 array `ms`, interpreted as the first block column.";
+blockmat2::usage = "blockmat2[mss] builds a generalized block Toeplitz matrix from a rank-4 array `mss` of consecutive block columns.";
+sylvester1::usage = "sylvester1[ps, np, qs, nq] returns the two block rows used to form a Sylvester-type matrix. sylvester1[ps, qs] uses the default shifts determined by the input lengths.";
+todata::usage = "todata[xs] converts a sparse tensor or matrix into FiniteFlow-style coordinate data `{positions, values}`.";
+todataDense::usage = "todataDense[xs] converts a tensor or matrix into dense FiniteFlow-style data with row-major positions and flattened values.";
+relabel::usage = "relabel[m] replaces the nonzero entries of `m` by consecutive labels in row-major order, preserving the sparsity pattern.";
+keepnz::usage = "keepnz[learn][m] keeps only the entries of `m` selected by the sparsity information stored in `learn`. Option `\"Filter\"` preprocesses the coordinate-value list.";
+laurent::usage = "laurent[learn][m] expands sparse data into an additional Laurent-coefficient axis according to the exponent ranges stored in `learn`.";
+sparsesol::usage = "sparsesol[learn][m] reconstructs a sparse solution matrix from learned dependent-variable relations.";
+densesol::usage = "densesol[learn, needed][m] reconstructs a dense solution matrix from learned dependent-variable relations, keeping `needed` leading variables.";
+sepIndep::usage = "sepIndep[indep][m] splits the last dimension of `m` into trailing independent entries and the remaining dependent entries.";
+reconstruct::usage = "reconstruct[data][values] rebuilds a sparse tensor from the coordinate tensor `data` and a matching array of `values`.";
+add::usage = "add[datas] merges several sparse data rule lists by common coordinates.";
+dot::usage = "dot[dataLeft, dataRight] contracts two sparse data tensors along their shared dimension and returns grouped sparse product data.";
+joinSys::usage = "joinSys[main, phil, phir] embeds three block matrices into one larger coupled linear system layout.";
+plot::usage = "plot[topMargin, rightMargin, size][data] renders a debugging view of sparse block structure as ASCII art.";
 
-(* Block Toeplitz matrix with `ms` as the first block column *)
-blockmat[ms:(_List | _SparseArray)] /; SameQ[ms // Dimensions // Length, 3] := ms // RightComposition[
+Begin["`Private`"]
+
+(* Block Toeplitz matrix with `ms` as the first block column. *)
+ClearAll[blockmat];
+blockmat[ms:(_List | _SparseArray)] /; SameQ[Dimensions[ms] // Length, 3] := ms // RightComposition[
     Length,
     Range,
-    Map[ms[[;;-#]]& /* (PadLeft[#, ms // Dimensions]&)],
+    Map[ms[[;; -#]]& /* (PadLeft[#, Dimensions[ms]]&)],
     SparseArray,
     Flatten[#, {{2, 3}, {1, 4}}]&,
-    (* ArrayRules, *)
-    (* Most, *)
     Identity
 ];
 
-(* Generalized block Toeplitz matrix with `mss` as consecutive block columns *)
+(* Generalized block Toeplitz matrix with `mss` as consecutive block columns. *)
+ClearAll[blockmat2];
 blockmat2[mss:(_List | _SparseArray)] /; And[
-    SameQ[mss // Dimensions // Length, 4],
-    mss // Dimensions // Part[#, ;;2]& // Apply[SameQ],
+    SameQ[Dimensions[mss] // Length, 4],
+    Dimensions[mss][[;; 2]] // Apply[SameQ],
     True
 ] := mss // RightComposition[
-    MapIndexed[#1[[;;-#2[[1]]]]& /* (PadLeft[#, mss // Dimensions // Rest]&)],
+    MapIndexed[#1[[;; -#2[[1]]]]& /* (PadLeft[#, Rest[Dimensions[mss]]]&)],
     SparseArray,
     Flatten[#, {{2, 3}, {1, 4}}]&,
-    (* ArrayRules, *)
-    (* Most, *)
     Identity
 ];
 
-(* ::Subsubsection::Closed:: *)
-(*Sylvester matrix*)
 
-
-ClearAll[sylvester1, sylvester0];
+(* Sylvester-type block rows for polynomial coefficient lists. *)
+ClearAll[sylvester1];
 sylvester1[
     ps:(_List | _SparseArray), np_Integer,
     qs:(_List | _SparseArray), nq_Integer
-] /; Length[ps] + np <= Length[qs] + nq:= List[
+] /; Length[ps] + np <= Length[qs] + nq := {
     np // RightComposition[
         Range,
         Map[PadLeft[ps, Length[ps] + # - 1]&],
         Reverse,
         PadRight,
-        (* PadRight[#, #[[1]] // Length // {#, #}&]&, *)
-        PadLeft[#, # // Dimensions // Add[{0, Length[qs] + nq - (Length[ps] + np)}]]&,
-        PadRight[#, # // Dimensions // Add[{nq, 0}]]&,
-        (* MatrixForm, *)
+        PadLeft[#, Dimensions[#] // utils`Add[{0, Length[qs] + nq - (Length[ps] + np)}]]&,
+        PadRight[#, Dimensions[#] // utils`Add[{nq, 0}]]&,
         Identity
     ],
     nq // RightComposition[
         Range,
         Map[PadLeft[qs, Length[qs] + # - 1]&],
         PadRight,
-        (* PadLeft[#, #[[1]] // Length // {#, #}&]&, *)
-        PadLeft[#, # // Dimensions // Add[{np, 0}]]&,
-        (* MatrixForm, *)
+        PadLeft[#, Dimensions[#] // utils`Add[{np, 0}]]&,
         Identity
-    ],
-    Nothing
-];
+    ]
+};
 sylvester1[ps:(_List | _SparseArray), qs:(_List | _SparseArray)] := sylvester1[
     ps, Length[qs] - 1,
     qs, Length[ps] - 1
-]
-(* FIXME old version, remove *)
-sylvester0[ps:(_List | _SparseArray), qs:(_List | _SparseArray)] := List[
-    qs // RightComposition[
-        Length,
-        Add[-1],
-        Range,
-        Map[PadLeft[ps, Length[ps] + # - 1]&],
-        (* PadRight, *)
-        Reverse,
-        PadRight[#, #[[1]] // Length // {#, #}&]&,
-        (* PadLeft[#, # // Dimensions // Add[{0, Length[qs] - 1}]]&, *)
-        (* MatrixForm, *)
-        Identity
-    ],
-    ps // RightComposition[
-        Length,
-        Add[-1],
-        Range,
-        Map[PadLeft[qs, Length[qs] + # - 1]&],
-        PadRight,
-        PadLeft[#, #[[1]] // Length // {#, #}&]&,
-        (* PadLeft[#, # // Dimensions // Add[{0, Length[qs] - 1}]]&, *)
-        (* (1* MatrixForm, *1) *)
-        Identity
-    ],
-    Nothing
-    (* (1* qs // RightComposition[ *1) *)
-    (* (1*     Length, *1) *)
-    (* (1*     Add[-1], *1) *)
-    (*     Range, *)
-    (*     DiagonalMatrix, *)
-    (*     (1* PadRight[#, # // Dimensions // Add[Length[ps] + Length[qs] - 2 // {#, *1) *)
-    (*     (1* #}&] // Add[{-Length[qs] + 1, 0}]]&, *1) *)
-    (*     (1* MatrixForm, *1) *)
-    (*     Identity *)
-    (* ] *)
 ];
 
-SameQ[
-    sylvester1[Range[8], 4, Alphabet[][[;;5]], 7],
-    sylvester0[Range[8], Alphabet[][[;;5]]]
-]
 
-SameQ[
-    sylvester1[Range[8], Alphabet[][[;;5]]],
-    sylvester0[Range[8], Alphabet[][[;;5]]]
-]
-
-(* ::Subsubsection::Closed:: *)
-(*tensor `data` for `FiniteFlow`*)
-
+(* Sparse tensor data in the format expected by FiniteFlow. *)
+ClearAll[todata, todataDense];
 todata[xs:(_List | _SparseArray)] := xs // RightComposition[
     ArrayRules,
     Most,
     SortBy[First],
     Map[Apply[List]],
     Transpose,
-    (* edge case for empty `data` *)
     If[SameQ[#, {}], {{}, {}}, #]&,
     MapAt[
-        Rule[#, # // Length // Range]& /* Thread /* (SparseArray[#, xs // Dimensions]&),
+        Rule[#, Length[#] // Range]& /* Thread /* (SparseArray[#, Dimensions[xs]]&),
         #,
         {1}
     ]&,
@@ -175,25 +100,26 @@ todataDense[xs:(_List | _SparseArray)] := xs // RightComposition[
             Dimensions,
             Apply[Times],
             Range,
-            ArrayReshape[#, xs // Dimensions]&,
+            ArrayReshape[#, Dimensions[xs]]&,
             SparseArray,
             Identity
         ],
-        # // Flatten // Normal,
-        Nothing
+        Normal[Flatten[#]]
     }&,
     Identity
 ];
 
-ClearAll @ relabel;
+
+(* Relabel each nonzero entry by its row-major position among nonzeros. *)
+ClearAll[relabel];
 relabel[m:(_List | _SparseArray)] := m // RightComposition[
     ArrayRules,
     Part[#, ;;-2, 1]&,
     (* this should enforce row-major order *)
     Sort,
-    Rule[#, # // Length // Range]&,
+    Rule[#, Length[#] // Range]&,
     Thread,
-    SparseArray[#, m // Dimensions]&,
+    SparseArray[#, Dimensions[m]]&,
     Identity
 ];
 
@@ -205,41 +131,22 @@ Options @ keepnz = {
 };
 keepnz[learn_, OptionsPattern[]][m:(_List | _SparseArray)] := m // RightComposition[
     Normal,
-    MapIndexed[#2 -> #1&, #, # // Dimensions // Length // List]&,
+    MapIndexed[#2 -> #1&, #, {Length[Dimensions[#]]}]&,
     Flatten,
     OptionValue["Filter"],
     Part[#, All, 1]&,
     Extract[#, learn[[2, 2]] // Map[List]]&,
-    Rule[#, # // Length // Range]&,
+    Rule[#, Length[#] // Range]&,
     Thread,
-    SparseArray[#, m // Dimensions]&,
+    SparseArray[#, Dimensions[m]]&,
     Identity
 ];
 
-(* ClearAll @ keepnz1 *)
-(* Options @ keepnz1 = { *)
-(*     (1* kinda a duct tape due to `FFAlgSparseMatMul` returning dense output *1) *)
-(*     "Filter" -> Identity, *)
-(*     Nothing *)
-(* }; *)
-(* keepnz1[learn_, OptionsPattern[]][m:(_List | _SparseArray)] := m // RightComposition[ *)
-(*     Normal, *)
-(*     MapIndexed[#2 -> #1&, #, # // Dimensions // Length // List]&, *)
-(*     Flatten, *)
-(*     OptionValue["Filter"], *)
-(*     SortBy[#, Last]&, *)
-(*     (1* Part[#, All, 1]&, *1) *)
-(*     Extract[#, learn[[2, 2]] // Map[List]]&, *)
-(*     (1* Rule[#, # // Length // Range]&, *1) *)
-(*     (1* Thread, *1) *)
-(*     (1* SparseArray[#, m // Dimensions]&, *1) *)
-(*     Identity *)
-(* ]; *)
 
-(* TODO avoid `Module` here? *)
-ClearAll @ laurent;
+(* Expand sparse data into a Laurent-coefficient axis. *)
+ClearAll[laurent];
 laurent[learn_][m:(_List | _SparseArray)] := Module[{min, max},
-    {min, max} = zipWith[{Min, Max}, learn];
+    {min, max} = utils`zipWith[{Min, Max}, learn];
     m // RightComposition[
         ArrayRules,
         Most,
@@ -249,68 +156,64 @@ laurent[learn_][m:(_List | _SparseArray)] := Module[{min, max},
         {
             #,
             learn // RightComposition[
-                Add[-min + 1],
+                utils`Add[-min + 1],
                 Transpose,
                 Map[Apply[Range]],
                 Identity
-            ],
-            Nothing
+            ]
         }&,
         Transpose,
-        Map[Apply[Function[{x, xs}, xs // Map[Append[x, #]&]]]],
+        Map[Apply[Function[{x, xs}, Map[Append[x, #]&, xs]]]],
         Flatten[#, 1]&,
-        (* Extract[#, learn[[2, 2]] // Map[List]]&, *)
-        Rule[#, # // Length // Range]&,
+        Rule[#, Length[#] // Range]&,
         Thread,
-        SparseArray[#, Append[m // Dimensions, Max[max - min + 1, 0]]]&,
+        SparseArray[#, Append[Dimensions[m], Max[max - min + 1, 0]]]&,
         Identity
     ]
 ];
 
-ClearAll @ sparsesol;
+
+(* Reconstruct sparse or dense solution matrices from learned relations. *)
+ClearAll[sparsesol, densesol];
 sparsesol[learn_][m:(_List | _SparseArray)] := learn // RightComposition[
-    Part[#, 1;;2, 2]&,
+    Part[#, 1 ;; 2, 2]&,
     Thread,
     Map[Thread],
     Apply[Join],
-    (* this works not as expected when the 2nd arg is a tensor, see
-     * Inner[f, {2, 1}, {{1}, {1}}, g]
-     *)
-    (* Inner[List /* Thread, #[[1, 2]], #[[2, 2]], Join]&, *)
-    Rule[#, # // Length // Range]&,
+    Rule[#, Length[#] // Range]&,
     Thread,
     SparseArray[
         #,
-        {learn[[1, 2]] // Length, m // Dimensions // Last}
+        {Length[learn[[1, 2]]], Dimensions[m][[-1]]}
     ]&,
     (* Remove the leading `DepVars` columns *)
     Part[#, All, Span[learn[[1, 2]] // Length // Add[1], All]]&,
     Identity
 ];
 
-ClearAll @ densesol;
 densesol[learn_, needed_Integer][m:(_List | _SparseArray)] := learn // RightComposition[
     Outer[List, #[[1, 2]], #[[2, 2]]]&,
     Flatten[#, 1]&,
-    Rule[#, # // Length // Range]&,
+    Rule[#, Length[#] // Range]&,
     Thread,
     SparseArray[
         #,
-        {needed, m // Dimensions // Last}
+        {needed, Dimensions[m][[-1]]}
     ]&,
-    (* Remove the leading `DepVars` columns *)
     Part[#, All, Span[needed + 1, All]]&,
     Identity
 ];
 
-ClearAll @ sepIndep;
+
+(* Split the trailing independent variables from the rest. *)
+ClearAll[sepIndep];
 sepIndep[indep_Integer] := RightComposition[
     Internal`PartitionRagged[
         #,
         {
-            {# // Length},
+            {Length[#]},
             {
-                # // Dimensions // Last // Add[-indep],
+                Dimensions[#][[-1]] - indep,
                 indep
             }
         }
@@ -320,7 +223,9 @@ sepIndep[indep_Integer] := RightComposition[
     Identity
 ];
 
-ClearAll @ reconstruct;
+
+(* Reconstruct a sparse tensor from stored coordinates and new values. *)
+ClearAll[reconstruct];
 reconstruct[data_] := RightComposition[
     Rule[
         data // RightComposition[
@@ -333,121 +238,125 @@ reconstruct[data_] := RightComposition[
         #
     ]&,
     Thread,
-    SparseArray[#, data // Dimensions]&,
+    SparseArray[#, Dimensions[data]]&,
     Identity
 ];
 
-ClearAll @ add;
+
+(* Group sparse data from multiple tensors by common coordinates. *)
+ClearAll[add];
 add[datas_List] := datas // RightComposition[
-    Transpose[#, # // Dimensions // Length // Range // RotateRight]&,
+    Transpose[#, RotateRight[Range[Length[Dimensions[#]]]]]&,
     ArrayRules,
     Most,
-    Map[#[[1, ;;-2]] -> {#[[1, -1]], #[[2]]}&],
+    Map[#[[1, ;; -2]] -> {#[[1, -1]], #[[2]]}&],
     GatherBy[#, First]&,
     Map[#[[1, 1]] -> #[[All, 2]]&],
     SortBy[First],
     Identity
 ];
 
-(* FIXME edge case when both datas are vectors *)
-ClearAll @ dot;
-Condition[
-    dot[dataLeft_SparseArray, dataRight_SparseArray],
-    And[
-        SameQ[
-            dataLeft  // Dimensions // Last,
-            dataRight // Dimensions // First
-        ],
-        True
-    ]
+
+(* Contract two sparse data tensors along one matching dimension. *)
+ClearAll[dot];
+dot[dataLeft_SparseArray, dataRight_SparseArray] /; SameQ[
+    Dimensions[dataLeft][[-1]],
+    Dimensions[dataRight][[1]]
 ] := {dataLeft, dataRight} // RightComposition[
     Map[ArrayRules /* Most],
     MapIndexed[Function[{list, index}, MapAt[{index[[1]], #}&, list, {All, 2}]], #]&,
     MapAt[GatherBy[#, Part[#, 1, -1]&]&, #, 1]&,
-    MapAt[GatherBy[#, Part[#, 1,  1]&]&, #, 2]&,
-    (* move the contracted index to the leftmost position in `dataLeft` *)
-    MapAt[Prepend[#[[;;-2]], #[[-1]]]&, #, {1, All, All, 1}]&,
+    MapAt[GatherBy[#, Part[#, 1, 1]&]&, #, 2]&,
+    MapAt[Prepend[#[[;; -2]], #[[-1]]]&, #, {1, All, All, 1}]&,
     Apply[Join],
-    (* first we do the multiplication *)
-    (* group by the contracted index value *)
     GatherBy[#, Part[#, 1, 1, 1]&]&,
-    (* only these groups correspond to non-zero contractions *)
     Cases[x_ /; Length[x] === 2],
-    Map[Apply[Outer[Rule[
-        Join[#1[[1, 2;;]], #2[[1, 2;;]]]
-    ,
-        Join[#1[[2]], #2[[2]]]
-    ]&, ##]&]],
+    Map[Apply[Outer[
+        Rule[
+            Join[#1[[1, 2 ;;]], #2[[1, 2 ;;]]],
+            Join[#1[[2]], #2[[2]]]
+        ]&,
+        ##]&
+    ]],
     Flatten,
-    (* now we do the addition *)
     GatherBy[#, First]&,
-    Map[Rule[
-        #[[1, 1]]
-    ,
-        #[[All, 2]]
-    ]&],
+    Map[#[[1, 1]] -> #[[All, 2]]&],
     SortBy[First],
     Map[MapAt[Sort, #, 2]&],
     Identity
 ];
 
-ClearAll @ joinSys;
+
+(* Embed a main block system together with left and right couplings. *)
+ClearAll[joinSys];
 joinSys[main_, phil_, phir_] := Module[{dimm, diml, dimr},
-    dimm = main // Dimensions;
-    diml = phil // Dimensions // Last;
-    dimr = phir // Dimensions // First;
+    dimm = Dimensions[main];
+    diml = Dimensions[phil][[-1]];
+    dimr = Dimensions[phir][[1]];
     {
         main // RightComposition[
-            PadRight[#, # // Dimensions // Add[{0, diml}]]&,
-            PadLeft[#, # // Dimensions // Add[{dimr, dimr}]]&,
+            PadRight[#, Dimensions[#] // utils`Add[{0, diml}]]&,
+            PadLeft[#, Dimensions[#] // utils`Add[{dimr, dimr}]]&,
             Identity
         ],
-        phil // PadLeft[#, # // Dimensions // Add[{dimr, dimm[[2]] + dimr}]]&,
+        PadLeft[phil, Dimensions[phil] + {dimr, dimm[[2]] + dimr}],
         phir // RightComposition[
-            PadLeft[#, # // Dimensions // Add[{0, dimr}]]&,
-            PadRight[#, # // Dimensions // Add[{dimm[[1]], diml}]]&,
+            PadLeft[#, Dimensions[#] // utils`Add[{0, dimr}]]&,
+            PadRight[#, Dimensions[#] // utils`Add[{dimm[[1]], diml}]]&,
             Identity
-        ],
-        Nothing
+        ]
     }
 ];
 
-(* ClearAll @ addIndex; *)
-(* addIndex[n_Integer] /; n > 0 := RightComposition[ *)
-(*     ArrayRules, *)
-(*     Most, *)
-(*     Map[#[[1]] -> {n, #[[2]]}&], *)
-(*     Identity *)
-(* ]; *)
 
-(* (1* https://mathematica.stackexchange.com/a/222572 *1) *)
-(* getcols[xs_SparseArray] := Internal`PartitionRagged[ *)
-(*     xs["ColumnIndices"], *)
-(*     Differences[xs["RowPointers"]] *)
-(* ]; *)
+(* Temporary ASCII visualization helper for debugging sparse layouts. *)
+ClearAll[plot];
+plot[topMargin_Integer, rightMargin_Integer, size_Integer][data_] := Internal`PartitionRagged[
+    data,
+    {
+        Flatten[{
+            topMargin,
+            ConstantArray[
+                Quotient[Dimensions[data][[1]] - topMargin, size],
+                size
+            ],
+            {}
+        }],
+        Flatten[{
+            topMargin,
+            ConstantArray[
+                Quotient[Dimensions[data][[-1]] - topMargin - rightMargin, size],
+                size
+            ],
+            rightMargin,
+            {}
+        }]
+    }
+] // utils`msS1;
 
-(* ::Subsubsection::Closed::*)
-(*plot*)
 
-(* This is just a temporary helper function for debugging *)
-ClearAll @ plot;
-plot[topMargin_Integer, rightMargin_Integer, size_Integer][data_] := Internal`PartitionRagged[data, {
-    Flatten[{
-        topMargin,
-        ConstantArray[
-            data // Dimensions // First // Add[-topMargin] // Quotient[#, size]&,
-            size
-        ],
-        {}
-    }],
-    Flatten[{
-        topMargin,
-        ConstantArray[
-            data // Dimensions // Last // Add[-topMargin - rightMargin] // Quotient[#, size]&,
-            size
-        ],
-        rightMargin,
-        {}
-    }]
-}] // msS1;
+SetAttributes[
+    {
+        blockmat,
+        blockmat2,
+        sylvester1,
+        todata,
+        todataDense,
+        relabel,
+        keepnz,
+        laurent,
+        sparsesol,
+        densesol,
+        sepIndep,
+        reconstruct,
+        add,
+        dot,
+        joinSys,
+        plot
+    },
+    ReadProtected
+];
 
+End[]
+
+EndPackage[]
